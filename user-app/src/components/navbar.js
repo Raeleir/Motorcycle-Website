427ed4207c42a8f8a58fa9220945677cb4f999a2
Joseph Carroll <sagamyr@gmail.com>
2017-08-07 14:37:29 -0600

    navbar

--- user-app/src/components/navbar.js
+++ user-app/src/components/navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
-
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
-
 class CustomNavbar extends React.Component {
     render() {
         return (
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/race">Race</Link>
-            </div>
+            <Navbar collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">Home</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <li>
+                            <Link to="/race">Race</Link>
+                        </li>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
